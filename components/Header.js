ecc3fe460c46e6cfc682af370f1e3be7ec12f032
Sebastien Green <45818412+sebygreen@users.noreply.github.com>
2023-06-16 12:35:06 +0200

    updated background & header

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,37 @@
 "use client";
 // dependencies
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 // components
 import Navigation from "../components/Navigation";
 import Gradient from "../components/Gradient";
 // styles
 import styles from "../styles/Header.module.css";
+// media
+import purple from "../public/assets/purple.png";
+
 export default function Header() {
     const pathname = usePathname();
     return (
         <div className={styles.header}>
+            <Image
+                className={styles.background}
+                src={purple}
+                alt="Topography type purple texture"
+            />
             <Navigation />
             <div className={styles.titlebar}>
-                <h1 className={styles.title}>{pathname == "/scrollmaps" ? "Scrollmap's" : pathname == "/cards" ? "Cards" : pathname == "/illustrations" ? "Illustrations" : pathname == "/extra" ? "Miscellaneous" : "Homepage"}</h1>
+                <h1 className={styles.title}>
+                    {pathname == "/scrollmaps"
+                        ? "Scrollmap's"
+                        : pathname == "/cards"
+                        ? "Cards"
+                        : pathname == "/illustrations"
+                        ? "Illustrations"
+                        : pathname == "/extra"
+                        ? "Miscellaneous"
+                        : "Homepage"}
+                </h1>
                 <div className={styles.tilt}>
                     <svg
                         viewBox="0 0 1440 120"
--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        appDir: true,
-    },
     images: {
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "xdwucvvincboyjolpqnx.supabase.co",
                 port: "",
-                pathname: "/storage/v1/object/sign/scrollmap-bucket/scrollmaps/**",
+                pathname:
+                    "/storage/v1/object/sign/scrollmap-bucket/scrollmaps/**",
             },
         ],
     },
